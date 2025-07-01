86e501d09d72a920137b869b1f5320247f1a85ab
Shkurin28 <skurinaolga02@gmail.com>
2025-07-01 23:10:21 +0300

    Update PZ_13.2.py

--- 13/PZ_13.2.py
+++ 13/PZ_13.2.py
@@ -2,10 +2,20 @@
 ﻿В двумерном списке все элементы, не лежащие на главной диагонали увеличить в 2 раза.
 """
 
-for i in range(len(matrix)):
-    for j in range(len(matrix[i])):
-        if i != j:  
-            matrix[i][j] *= 2
-print("\nМатрица после увеличения недиагональных элементов:")
-for row in matrix:
+
+def double_non_diagonal(matrix):
+    return [
+        list(map(lambda item: item * 2 if row != col else item, row_data))
+        for row, row_data in enumerate(matrix)
+        for col, item in enumerate(row_data)
+    ]
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+result = double_non_diagonal(matrix)
+for row in result:
     print(row)
